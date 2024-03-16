97486f66bd874f55a21b1da4976c8d139077ea52
sstepano <31989171+sstepano@users.noreply.github.com>
2024-03-16 22:19:14 +0100

    first

---
+++ src/main/java/com/example/conferencedemo/config/PersistanceConfiguration.java
@@ -0,0 +1,20 @@
+package com.example.conferencedemo.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersistanceConfiguration {
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+        builder.username("postgres");
+        builder.password("trogir");
+        System.out.println("My custom datasource bean has been initialized and set");
+        return builder.build();
+    }
+}
---
+++ src/main/java/com/example/conferencedemo/controllers/SpeakersControllers.java
@@ -0,0 +1,46 @@
+package com.example.conferencedemo.controllers;
+
+import com.example.conferencedemo.models.Speaker;
+import com.example.conferencedemo.repositories.SpeakerRepository;
+import jakarta.persistence.Lob;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
+public class SpeakersControllers {
+
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> list() {
+        return speakerRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id) {
+        return speakerRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Speaker create(@RequestBody Speaker speaker) {
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        speakerRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker) {
+        Speaker existingSpeaker = speakerRepository.getReferenceById(id);
+        BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+        return speakerRepository.saveAndFlush(speaker);
+    }
+}
